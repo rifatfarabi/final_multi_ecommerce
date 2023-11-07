e18d40c5fc637a26872cee689ee12fd59be330bf
rifat farabi <rifatfarabi08@gmail.com>
2023-11-07 09:59:24 +0600

    controller added

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerIndexController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Route;
 
@@ -35,3 +38,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/admin/login',[AdminLoginController::class,'adminLogin'])->name('admin.login');
 Route::get('/admin/dashboard',[AdminDashboardController::class,'adminDashboard'])->name('admin.dashboard');
 Route::get('/logout', [LoginController::class, 'logout']);
+
+Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('brands', BrandController::class);
